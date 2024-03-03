ab9458960ee480f5d95d53cc1cf275583edcf59a
tanvirshhub <tanvirshuvo712@gmail.com>
2024-03-03 21:48:05 +0600

    slider widget add

--- themfic-widget/assets/js/main.js
+++ themfic-widget/assets/js/main.js
@@ -1,7 +1,11 @@
-$(document).ready(function () {
-    $(".vertical-center").slick({
-        dots: true,
-        vertical: true,
-        centerMode: true,
-      });
+jQuery(document).ready(function($) {
+  // Initialize Slick Slider
+  $('.slick-slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      // Add more Slick options as needed
+  });
 });
--- themfic-widget/widgets/slider.php
+++ themfic-widget/widgets/slider.php
@@ -3,102 +3,181 @@
 class Elementor_Slider_Widget extends \Elementor\Widget_Base {
 
     public function get_name() {
-        return 'Theslider';
+        return 'elementor_slider_widget';
     }
 
     public function get_title() {
-        return esc_html__('Elementor Slider Widget', 'themefic_widget');
+        return esc_html__( 'Elementor Slider Widget', 'themefic_widget' );
     }
 
     public function get_icon() {
-        return 'eicon-post-slider';
+        return 'eicon-slides';
+    }
+
+    public function get_categories() {
+        return [ 'general' ];
     }
 
     protected function register_controls() {
         $this->start_controls_section(
-            'content_section',
+            'section_slides',
             [
-                'label' => esc_html__('Content', 'themefic_widget'),
-                'tab'   => \Elementor\Controls_Manager::TAB_CONTENT,
+                'label' => esc_html__( 'Slides', 'themefic_widget' ),
             ]
         );
 
-        // Add controls for multiple sliders using repeater
+        $repeater = new \Elementor\Repeater();
+
+        $repeater->add_control(
+            'image',
+            [
+                'label' => esc_html__( 'Image', 'themefic_widget' ),
+                'type' => \Elementor\Controls_Manager::MEDIA,
+            ]
+        );
+
+        $repeater->add_control(
+            'overlay_color',
+            [
+                'label' => esc_html__( 'Overlay Color', 'themefic_widget' ),
+                'type' => \Elementor\Controls_Manager::COLOR,
+                'selectors' => [
+                    '{{WRAPPER}} .slick-slide .overlay' => 'background-color: {{VALUE}};',
+                ],
+            ]
+        );
+
+        $repeater->add_control(
+            'icon_list',
+            [
+                'label' => esc_html__( 'Icon List', 'themefic_widget' ),
+                'type' => \Elementor\Controls_Manager::REPEATER,
+                'fields' => [
+                    [
+                        'name' => 'icon',
+                        'label' => esc_html__( 'Icon', 'themefic_widget' ),
+                        'type' => \Elementor\Controls_Manager::ICON,
+                    ],
+                    [
+                        'name' => 'text',
+                        'label' => esc_html__( 'Text', 'themefic_widget' ),
+                        'type' => \Elementor\Controls_Manager::TEXT,
+                        'default' => esc_html__( 'Your Text Here', 'themefic_widget' ),
+                    ],
+                ],
+                'default' => [
+                    [
+                        'icon' => 'fa fa-star',
+                        'text' => esc_html__( 'Star', 'themefic_widget' ),
+                    ],
+                    [
+                        'icon' => 'fa fa-heart',
+                        'text' => esc_html__( 'Heart', 'themefic_widget' ),
+                    ],
+                    // Add more default items as needed
+                ],
+            ]
+        );
+
+        $repeater->add_control(
+            'subheading',
+            [
+                'label' => esc_html__( 'Subheading', 'themefic_widget' ),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'default' => esc_html__( 'Your Subheading Here', 'themefic_widget' ),
+            ]
+        );
+
+        $repeater->add_control(
+            'main_heading',
+            [
+                'label' => esc_html__( 'Main Heading', 'themefic_widget' ),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'default' => esc_html__( 'Your Main Heading Here', 'themefic_widget' ),
+            ]
+        );
+
+        $repeater->add_control(
+            'button_text',
+            [
+                'label' => esc_html__( 'Button Text', 'themefic_widget' ),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'default' => esc_html__( 'Read More', 'themefic_widget' ),
+            ]
+        );
+
+        $repeater->add_control(
+            'share_icons',
+            [
+                'label' => esc_html__( 'Share Icons', 'themefic_widget' ),
+                'type' => \Elementor\Controls_Manager::ICONS,
+            ]
+        );
+
+        $repeater->add_control(
+            'before_button_text',
+            [
+                'label' => esc_html__( 'Before Button Text', 'themefic_widget' ),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'default' => esc_html__( 'Before', 'themefic_widget' ),
+            ]
+        );
+
+        $repeater->add_control(
+            'next_button_text',
+            [
+                'label' => esc_html__( 'Next Button Text', 'themefic_widget' ),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'default' => esc_html__( 'Next', 'themefic_widget' ),
+            ]
+        );
+
         $this->add_control(
             'slides',
             [
-                'label'   => esc_html__('Slides', 'themefic_widget'),
-                'type'    => \Elementor\Controls_Manager::REPEATER,
-                'fields'  => $this->get_slider_fields(),
-                'default' => [
-                    [
-                        'image' => [
-                            'url' => 'http://placehold.it/350x300?text=1',
-                        ],
-                    ],
-                    [
-                        'image' => [
-                            'url' => 'http://placehold.it/350x300?text=2',
-                        ],
-                    ],
-                    [
-                        'image' => [
-                            'url' => 'http://placehold.it/350x300?text=3',
-                        ],
-                    ],
-                    // Add more default slides as needed
-                ],
-                'title_field' => '{{{ image.url }}}',
+                'type' => \Elementor\Controls_Manager::REPEATER,
+                'fields' => $repeater->get_controls(),
+                'title_field' => '{{{ main_heading }}}',
             ]
         );
 
         $this->end_controls_section();
     }
 
-    protected function get_slider_fields() {
-        return [
-            [
-                'name'  => 'image',
-                'label' => esc_html__('Image', 'themefic_widget'),
-                'type'  => \Elementor\Controls_Manager::MEDIA,
-            ],
-            [
-                'name'  => 'subheading',
-                'label' => esc_html__('Subheading', 'themefic_widget'),
-                'type'  => \Elementor\Controls_Manager::TEXT,
-            ],
-            [
-                'name'  => 'main_heading',
-                'label' => esc_html__('Main Heading', 'themefic_widget'),
-                'type'  => \Elementor\Controls_Manager::TEXT,
-            ],
-            [
-                'name'  => 'button_text',
-                'label' => esc_html__('Button Text', 'themefic_widget'),
-                'type'  => \Elementor\Controls_Manager::TEXT,
-            ],
-            // Add other controls as needed for each slide
-        ];
-    }
-
     protected function render() {
         $settings = $this->get_settings_for_display();
-
-        // Render slides with repeater data
         ?>
-        <section class="center slider">
-            <?php foreach ($settings['slides'] as $slide) : ?>
-                <div>
-                    <img src="<?php echo esc_url($slide['image']['url']); ?>">
-                    <h3><?php echo esc_html($slide['subheading']); ?></h3>
-                    <h2><?php echo esc_html($slide['main_heading']); ?></h2>
-                    <a href="#" class="your-button-class"><?php echo esc_html($slide['button_text']); ?></a>
-                    <!-- Add other elements for each slide as needed -->
-                </div>
-            <?php endforeach; ?>
-        </section>
+        <div class="slick-slider">
+        <?php foreach ( $settings['slides'] as $slide ) : ?>
+            <div class="slick-slide">
+                <?php if ( ! empty( $slide['image']['url'] ) ) : ?>
+                    <div class="slide-image" style="background-image: url('<?php echo esc_url( $slide['image']['url'] ); ?>');">
+                        <div class="overlay" style="background-color: <?php echo esc_attr( $slide['overlay_color'] ); ?>;"></div>
+                    </div>
+                <?php endif; ?>
+                <h3><?php echo esc_html( $slide['subheading'] ); ?></h3>
+                <h2><?php echo esc_html( $slide['main_heading'] ); ?></h2>
+                <ul class="icon-list">
+                    <?php foreach ( $slide['icon_list'] as $item ) : ?>
+                        <li>
+                            <i class="<?php echo esc_attr( $item['icon'] ); ?>"></i>
+                            <?php echo esc_html( $item['text'] ); ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+                <a href="#" class="your-button-class"><?php echo esc_html( $slide['button_text'] ); ?></a>
+
+                <?php if ( ! empty( $slide['share_icons']['value'] ) ) : ?>
+                    <div class="share-icons"><?php echo $slide['share_icons']['value']; ?></div>
+                <?php endif; ?>
+
+                <button class="before-button"><?php echo esc_html( $slide['before_button_text'] ); ?></button>
+                <button class="next-button"><?php echo esc_html( $slide['next_button_text'] ); ?></button>
+            </div>
+        <?php endforeach; ?>
+        </div>
         <?php
     }
 }
 
-\Elementor\Plugin::instance()->widgets_manager->register_widget_type(new Elementor_Slider_Widget());
+\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Elementor_Slider_Widget() );
